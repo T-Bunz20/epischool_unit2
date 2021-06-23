216233f7bbabd6c1fded9c086b029aa1d840da07
T-Bunz20 <56098144+T-Bunz20@users.noreply.github.com>
2021-06-23 16:22:06 -0700

    Update Primes.py

--- Primes.py
+++ Primes.py
@@ -5,3 +5,10 @@ def is_prime(num):
         if(num % i == 0):
             return False
     return True
+
+def primes_below(num):
+    output = []
+    for i in range(2,num):
+        if is_prime(i):
+            output.append(i)
+    return output
